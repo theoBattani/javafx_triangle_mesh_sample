72b6d77a7a18650e16a3f678e7de10fa5220a14b
theoBattani <84023706+theoBattani@users.noreply.github.com>
2021-10-28 17:35:45 +0200

    ...

--- src/main/java/fr/theo/ViewController.java
+++ src/main/java/fr/theo/ViewController.java
@@ -12,12 +12,11 @@ import javafx.fxml.FXML;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
-import javafx.scene.Camera;
-import javafx.scene.PerspectiveCamera;
 import javafx.scene.shape.TriangleMesh;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.MeshView;
+import javafx.scene.shape.CullFace;
 import javafx.scene.shape.DrawMode;
 
 public class ViewController {
@@ -35,12 +34,8 @@ public class ViewController {
 
   // This method is called by the FXMLLoader when initialization is complete
   @FXML void initialize() {
-    Camera camera = new PerspectiveCamera();
-    camera.setNearClip(0);
-    camera.setFarClip(10000);
-    camera.translateXProperty().set(-scene.getWidth() / 2);
-    camera.translateYProperty().set(scene.getHeight() / 2);
-    camera.translateZProperty().set(-500);
+    stage.setWidth(640);
+    stage.setHeight(640);
     TriangleMesh mesh = new TriangleMesh();
     mesh.getTexCoords().addAll(0, 0);
     float h = 150;
@@ -63,11 +58,14 @@ public class ViewController {
     PhongMaterial material = new PhongMaterial();
     material.setDiffuseColor(Color.DARKBLUE);
     material.setSpecularColor(Color.BLUE);
-    MeshView view = new MeshView(mesh);
-    view.setDrawMode(DrawMode.FILL);
-    view.setMaterial(material);
-    group.getChildren().addAll(view, camera);
-    scene.setRoot(group);
+    MeshView meshView = new MeshView(mesh);
+    meshView.setDrawMode(DrawMode.FILL);
+    meshView.setMaterial(material);
+    meshView.setCullFace(CullFace.BACK);
+    meshView.setTranslateX(stage.getWidth() / 2);
+    meshView.setTranslateY(stage.getHeight() / 2);
+    meshView.setTranslateZ(1);
+    group.getChildren().addAll(meshView);
   }
 }
 
